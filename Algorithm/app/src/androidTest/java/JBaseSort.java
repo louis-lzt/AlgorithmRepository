cacdca5cfdf34de1eff6a9b76d9ff44b6a1d2e5e
zhaotian.liu <zhaotian.liu@tcl.com>
2022-04-14 09:54:53 +0800

    feat,添加 归并排序

--- Algorithm/app/src/androidTest/java/JBaseSort.java
+++ Algorithm/app/src/androidTest/java/JBaseSort.java
@@ -147,14 +147,20 @@ public class JBaseSort {
 
     private static void merge(int[] arr, int l, int mid, int r) {
         int[] help = new int[r-l + 1];
-        int i = 0;
+        int i = 0;  // 专门给help使用的
         int p1 = l;
         int p2 = mid + 1 ;
         while(p1 <= mid && p2<=r){
             help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
         }
         while(p1 <= mid){
-
+            help[i++] = arr[p1++];
+        }
+        while(p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for(i = 0; i< help.length; i++){
+            arr[l+i] = help[i];
         }
     }
 
