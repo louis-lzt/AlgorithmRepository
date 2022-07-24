f8e23d5d99d33ca9b9d150d3d379ef50cff4d649
zhaotian.liu <zhaotian.liu@tcl.com>
2022-07-24 19:43:36 +0800

    feat,添加 堆排序

--- Algorithm/app/src/androidTest/java/JBaseSort.java
+++ Algorithm/app/src/androidTest/java/JBaseSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * dec: 静态内部类单例 基础排序
@@ -9,10 +10,46 @@ import java.util.Arrays;
  */
 public class JBaseSort {
 
+    // 堆排序扩展题目
+    // 已知一个几乎有序的数组，几乎有序是指，如果把数组排好顺序的话，每个元素移动的距离可以不超过k，并且k相对于
+    //数组来说比较小。请选一个合适的排序算法针对这个数据进行排序
+    public void sortedArrDistanceLessK(int[] arr, int k) {
+        // 默认小根堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int index = 0;
+        for (; index <= Math.min(arr.length, k); index++) {
+            heap.add(arr[index]);
+        }
+        int i = 0;
+        for (; index < arr.length; i++, index++) {
+            heap.add(arr[index]);
+            arr[i] = heap.poll();
+        }
+        while(!heap.isEmpty()){
+            arr[i++] = heap.poll();
+        }
+    }
+
+    // 堆排序
+    public static void heapsort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) { // O(N)
+            heapInsert(arr, i);   // O(logN)
+        }
+        int heapSize = arr.length;
+        swap1(arr, 0, --heapSize);
+        while (heapSize > 0) { // O(N)
+            heapify(arr, 0, heapSize); // O(logN)
+            swap1(arr, 0, --heapSize);
+        }
+    }
+
     // 某个数现在处在index位置，往上继续移动
     public static void heapInsert(int[] arr, int index) {
         while (arr[index] > arr[(index - 1)] / 2) {
-            swap(arr, index, (index - 1) / 2);
+            swap1(arr, index, (index - 1) / 2);
             index = (index - 1) / 2;
         }
     }
@@ -21,14 +58,14 @@ public class JBaseSort {
         int left = index * 2 + 1; // 左孩子的下标
         while (left < heapSize) { // 下方还有孩子的时候
 
-            // 两个孩子中，谁的值大，把下表给largest
+            // 两个孩子中，谁的值大，把下标给largest
             int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
             // 父和孩子之间，谁的值大，就把下标 给 largest
             largest = arr[largest] > arr[index] ? largest : index;
             if (largest == index) {
                 break;
             }
-            swap(arr, largest, index);
+            swap1(arr, largest, index);
             index = largest;
             left = index * 2 + 1;
         }
@@ -68,14 +105,14 @@ public class JBaseSort {
         int more = r;
         while (l < more) {
             if (arr[l] < arr[r]) {
-//                swap(arr, ++less, l++);
+                swap1(arr, ++less, l++);
             } else if (arr[l] > arr[r]) {
-//                swap(arr, --more, less++);
+                swap1(arr, --more, less++);
             } else {
                 l++;
             }
         }
-//        swap(arr, --more, r);
+        swap1(arr, --more, r);
         return arr;
     }
 
